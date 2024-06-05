727644f8ae393e3d25d3eec1a531e514eaf5a424
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-06-05 21:15:04 +0300

    chore: some updates on doctor and lab  services

--- middlewares/isDoc.isLab.js
+++ middlewares/isDoc.isLab.js
@@ -4,7 +4,7 @@ const { User } = require("../models/user.model");
 async function UserPrivileges(req, res, next) {
   try {
     if (!req.header("Authorization")) {
-      return res.status(401).json("FORBIDDEN");
+      return res.status(401).json("authorization header not found");
     }
     const key = req.header("Authorization").split(" ")[0];
     const token = req.header("Authorization").split(" ")[1];
@@ -14,16 +14,16 @@ async function UserPrivileges(req, res, next) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded._id);
     if (!user) {
-      return res.status(401).json("FORBIDDEN");
+      return res.status(401).json("user not found");
     }
     if (user.role !== "DOC" && user.role !== "LAB") {
-      return res.status(401).json("FORBIDDEN");
+      return res.status(401).json("user shouldbe a DOC or a LAB");
     }
     req.userId = decoded._id;
     next();
   } catch (error) {
     console.log(error.message);
-    return res.status(401).json("FORBIDDEN");
+    return res.status(500).json("local server error");
   }
 }
 
